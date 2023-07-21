615a39bc0cd2177f62e3c2e12d1d1f9c69d42387
ursister <106648827+ursisterbtw@users.noreply.github.com>
2023-07-20 20:51:45 -0400

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,36 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-black text-white">
-      <a target="_blank" href="https://twitter.com/ursisterbtw">
+    <header className="flex justify-between items-center p-4 bg-black text-white overflow-hidden">
+      <motion.a
+        target="_blank"
+        href="https://twitter.com/ursisterbtw"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
         <img
           src="/twitter-icon.png"
           alt="Twitter"
           style={{ height: 25, width: 25, filter: "brightness(0) invert(1)" }}
         />
-      </a>
+      </motion.a>
       <h1>ursister's dashboard</h1>
-      <a target="_blank" href="https://github.com/ursisterbtw">
+      <motion.a
+        target="_blank"
+        href="https://github.com/ursisterbtw"
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
         <img
           src="/github-icon.png"
           alt="GitHub"
           style={{ height: 25, width: 25, filter: "brightness(0) invert(1)" }}
         />
-      </a>
+      </motion.a>
     </header>
   );
 };
