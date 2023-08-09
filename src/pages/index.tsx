651e81e0a9163b2766debbd0e477873a04ae66fe
ursister <106648827+ursisterbtw@users.noreply.github.com>
2023-08-09 10:38:03 -0400

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
         transition={{ duration: 1, ease: "easeOut" }} // Ease out for a smoother transition
         className="col-span-2"
       >
-        <Module4 />
+        <Module3 />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, x: -100 }}
